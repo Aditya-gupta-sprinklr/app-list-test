222360f85207402d2462129936fb59ff583052c4
Aditya-gupta-sprinklr <134982656+Aditya-gupta-sprinklr@users.noreply.github.com>
2023-05-30 11:16:58 +0530

    Add files via upload

--- first.js
+++ first.js
@@ -1,20 +1,38 @@
 function myFunction(e){
     let arr=[];
     let element=e.target;
-    console.log(element);
-    console.log(element.nodeName);
+    // console.log(element);
+    // console.log(element.nodeName);
     if(element.nodeName=="LABEL" || element.nodeName=="IMG")
     {
         element=element.parentNode;
     }
     let img_src_tobeshown=element.childNodes[0].src;
-    console.log(img_src_tobeshown,);
+    //console.log(img_src_tobeshown,);
 
     let trgt_element=document.querySelector(".flex-item-right").childNodes[0];
-    console.log(trgt_element);
+    //console.log(trgt_element);
 
     if(img_src_tobeshown==undefined)
         return;
     trgt_element.src=img_src_tobeshown;
 
+
+    
+
+
+}
+
+
+//coloring highlighted div
+function colorr(x)
+{
+    console.log(x);
+
+    x.style.backgroundColor="rgb(173,216,230)";
+}
+
+function uncolorr(x)
+{
+    x.style.backgroundColor="rgb(237,228,228)";
 }
